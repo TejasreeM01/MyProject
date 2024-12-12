94bbf26f3dff9e3ace17612646f2b01bd3e2cd01
TejasreeM01 <madipojutejasree@gmail.com>
2024-12-12 12:22:57 +0000

    project upload

---
+++ Fabric-network/startHealthcareNetwork.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+echo "------------Register the ca admin for each organization—----------------"
+
+docker compose -f docker/docker-compose-ca.yaml up -d
+sleep 3
+
+sudo chmod -R 777 organizations/
+
+echo "------------Register and enroll the users for each organization—-----------"
+
+chmod +x registerEnroll.sh
+
+./registerEnroll.sh
+sleep 3
+
+echo "—-------------Build the infrastructure—-----------------"
+
+docker compose -f docker/docker-compose-3org.yaml up -d
+sleep 3
+
+echo "-------------Generate the genesis block—-------------------------------"
+
+export FABRIC_CFG_PATH=${PWD}/config
+
+export CHANNEL_NAME=healthcarechannel
+
+configtxgen -profile ThreeOrgsChannel -outputBlock ${PWD}/channel-artifacts/${CHANNEL_NAME}.block -channelID $CHANNEL_NAME
+sleep 2
+
+echo "------ Create the application channel------"
+
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/healthcare.com/orderers/orderer.healthcare.com/msp/tlscacerts/tlsca.healthcare.com-cert.pem
+
+export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/healthcare.com/orderers/orderer.healthcare.com/tls/server.crt
+
+export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/healthcare.com/orderers/orderer.healthcare.com/tls/server.key
+
+osnadmin channel join --channelID $CHANNEL_NAME --config-block ${PWD}/channel-artifacts/$CHANNEL_NAME.block -o localhost:7053 --ca-file $ORDERER_CA --client-cert $ORDERER_ADMIN_TLS_SIGN_CERT --client-key $ORDERER_ADMIN_TLS_PRIVATE_KEY
+sleep 2
+
+osnadmin channel list -o localhost:7053 --ca-file $ORDERER_CA --client-cert $ORDERER_ADMIN_TLS_SIGN_CERT --client-key $ORDERER_ADMIN_TLS_PRIVATE_KEY
+sleep 2
+
+export FABRIC_CFG_PATH=${PWD}/peercfg
+export CORE_PEER_LOCALMSPID=HospitalsMSP
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/hospitals.healthcare.com/peers/peer0.hospitals.healthcare.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/hospitals.healthcare.com/users/Admin@hospitals.healthcare.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+export HOSPITALS_PEER_TLSROOTCERT=${PWD}/organizations/peerOrganizations/hospitals.healthcare.com/peers/peer0.hospitals.healthcare.com/tls/ca.crt
+export LABS_PEER_TLSROOTCERT=${PWD}/organizations/peerOrganizations/labs.healthcare.com/peers/peer0.labs.healthcare.com/tls/ca.crt
+export INSURANCECOMPANY_PEER_TLSROOTCERT=${PWD}/organizations/peerOrganizations/insurancecompany.healthcare.com/peers/peer0.insurancecompany.healthcare.com/tls/ca.crt
+sleep 2
+
+echo "—---------------Join Hospitals peer to the channel—-------------"
+
+echo ${CORE_PEER_TLS_ROOTCERT_FILE}
+echo ${CORE_PEER_MSPCONFIGPATH}
+echo ${CORE_PEER_ADDRESS}
+echo ${FABRIC_CFG_PATH}
+sleep 2
+peer channel join -b ${PWD}/channel-artifacts/${CHANNEL_NAME}.block
+sleep 3
+
+echo "-----channel List----"
+peer channel list
+
+echo "—-------------Hospitals anchor peer update—-----------"
+
+peer channel fetch config ${PWD}/channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.healthcare.com -c $CHANNEL_NAME --tls --cafile $ORDERER_CA
+sleep 1
+
+cd channel-artifacts
+
+configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
+jq '.data.data[0].payload.data.config' config_block.json > config.json
+
+cp config.json config_copy.json
+
+jq '.channel_group.groups.Application.groups.HospitalsMSP.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "peer0.hospitals.healthcare.com","port": 7051}]},"version": "0"}}' config_copy.json > modified_config.json
+
+configtxlator proto_encode --input config.json --type common.Config --output config.pb
+configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
+configtxlator compute_update --channel_id ${CHANNEL_NAME} --original config.pb --updated modified_config.pb --output config_update.pb
+
+configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
+echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL_NAME'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . > config_update_in_envelope.json
+configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output config_update_in_envelope.pb
+
+cd ..
+
+peer channel update -f ${PWD}/channel-artifacts/config_update_in_envelope.pb -c $CHANNEL_NAME -o localhost:7050  --ordererTLSHostnameOverride orderer.healthcare.com --tls --cafile $ORDERER_CA
+sleep 1
+
+
+
